82e491a7b4d05b8ab77d22145bdbb05467d34723
Jeff Emanuel <jeff141421@gmail.com>
2026-08-24 14:24:23 -0400

    chore(manifest): sync internal checksums ledger

--- scripts/generated/internal_checksums.sh
+++ scripts/generated/internal_checksums.sh
@@ -10,7 +10,7 @@
 ACFS_INTERNAL_CHECKSUMS_SCHEMA=1
 
 declare -gA ACFS_INTERNAL_CHECKSUMS=(
-  [install.sh]="d7019bb68171c48654a52329f7b78ab7fcb5930e54f68263a21e90fc5c123f05"
+  [install.sh]="0b41722ba163f1dca24d1a8e7c590e20bcb977eb8f6a1028647255e20f655e55"
   [checksums.yaml]="a62d703fe6d1953b22db8db249538c8a29eff8955f6e555ca4472fb2ee00a0e3"
   [scripts/preflight.sh]="6c49cbb7368c52cbfb720b2781e1b335379a88e151ce24a5b386e9b91b99273c"
   [scripts/lib/security.sh]="8e1429aba5c6e97e9502597857892ec1e11f367d8aee1336469ab3ccfa7081d7"
